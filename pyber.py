010b762b45d8ae46e31bffeeebc0c5ad58995d81
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-07-27 17:13:48 -0500

    added comments

--- pyber.py
+++ pyber.py
@@ -14,7 +14,7 @@ COL = namedtuple(
     "Columns", "CITY TYPE FARE DRIVERS RIDE",
     defaults=["city", "type", "fare", "driver_count", "ride_id"]
 )()
-TYPE = namedtuple(
+CITY_TYPES = namedtuple(
     "CityTypes", "URBAN SUBURBAN RURAL", 
     defaults=["Urban", "Suburban", "Rural"]
 )()
@@ -25,7 +25,7 @@ pyber_data_df = city_data_df.merge(ride_data_df, on=[COL.CITY, COL.CITY]).set_in
 # Separate data per city type and group by city
 type_groups = {
     typ: pyber_data_df[pyber_data_df[COL.TYPE] == typ].groupby([COL.CITY])
-    for typ in TYPE
+    for typ in CITY_TYPES
 }
 def get_count_by_city_by_type(col: str) -> dict:
     """Return dictionary of city types with values of
@@ -42,7 +42,23 @@ ride_count = get_count_by_city_by_type(COL.RIDE)
 fare_average = get_average_by_city_by_type(COL.FARE)
 drivers_average = get_average_by_city_by_type(COL.DRIVERS)
 
-def create_bubble_chart(ax: plt.Axes, city_type: str, color: str):
+def create_bubble_chart(ax: plt.Axes, city_type: str, color: str) -> plt.Axes:
+    """
+    Generate a scatter-bubble chart in the specified axis.
+    This depends on previously made dictionaries where
+    the key is city type and the value is a pandas series.
+
+    Each pandas series will contain the data for the x, y, s
+    scatter plot arguments.
+
+    Args:
+        ax (plt.Axes): Axis on which to generate the plot.
+        city_type (str): Key for dictionary.
+        color (str): Chosen color for this plot.
+
+    Returns:
+        plt.Axes: The same axis reference.
+    """
     ax.scatter(
         ride_count[city_type], 
         fare_average[city_type],
@@ -57,7 +73,6 @@ def create_bubble_chart(ax: plt.Axes, city_type: str, color: str):
     ax.set_xlabel("Total Number of Rides (Per City)")
     ax.set_ylabel("Average Fare ($)")
     ax.grid(True)
-    # ax.legend()
     legend = ax.legend(fontsize="12", mode="Expanded",
          scatterpoints=1, loc="best", title="City Type")
     for lg in legend.legendHandles:
@@ -66,10 +81,13 @@ def create_bubble_chart(ax: plt.Axes, city_type: str, color: str):
     return ax
 
 # %%
-# Plots
+# Creating multiple figures to save various plots.
+# Each figure uses a different city type and color.
+# We create a new axis per figure everytime, add text to it
+# and finally save to disk.
 figures = {
     typ: plt.figure(figsize=(8,5)) for i in range(0,3)
-    for typ in TYPE
+    for typ in CITY_TYPES
 }
 colors = ["coral", "skyblue", "gold"]
 text_kwargs = {
@@ -86,10 +104,15 @@ for typ, color in zip(figures, colors):
     figures[typ].savefig(f"ridesharing_{typ}.jpeg", dpi=300, bbox_inches='tight')
 
 # %%
-# Combined plot
+# Generating multiple plots in the same axis,
+# The variatons are city types and colors.
+# Once created, write text to figure and save.
 fig = plt.figure(figsize=(8,5))
 ax1 = fig.add_subplot(111)
-for typ, color in zip(TYPE, colors):
+for typ, color in zip(CITY_TYPES, colors):
     ax = create_bubble_chart(ax1, typ, color)
 fig.text(**text_kwargs)
 fig.savefig(f"ridesharing.jpeg", dpi=300, bbox_inches='tight')
+
+# %%
+type_groups[CITY_TYPES.URBAN].describe().describe()
