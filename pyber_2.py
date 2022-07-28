928a514effed92537df21b5801c9e92871641454
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-07-28 00:46:45 -0500

    added cells to pyber_2.py

--- pyber_2.py
+++ pyber_2.py
@@ -1,19 +1,22 @@
+"""
+Pyber
+"""
+# %%
 # Setup dataframes and functions
 import pandas as pd
 from pathlib import Path
 from pyberlib import Pyber
 
-
-# Load data
+# %%
+# Load data and merge data
 resources = Path("resources")
 output_path = Path("analysis")
 city_data_df = pd.read_csv(resources / "city_data.csv")
 ride_data_df = pd.read_csv(resources / "ride_data.csv")
-
-# Merge data
 pyber_data_df = city_data_df.merge(ride_data_df, on=[Pyber.COL.CITY, Pyber.COL.CITY])
 pyber_data_df = pyber_data_df.set_index(Pyber.COL.CITY)
 
+# %%
 # Separate data per City Type
 city_types_dict = {
     typ: pyber_data_df[pyber_data_df[Pyber.COL.TYPE] == typ] 
@@ -25,17 +28,20 @@ driver_count = {typ: city_types_dict[typ][Pyber.COL.DRIVERS] for typ in Pyber.ci
 fare_average = Pyber.get_averages(city_types_dict, Pyber.COL.FARE)
 drivers_average = Pyber.get_averages(city_types_dict, Pyber.COL.DRIVERS)
 
+# %%
 # Get stats
 ride_count_stats = Pyber.get_stats(ride_count)
 fare_stats = Pyber.get_stats(fare_count)
 drivers_stats = Pyber.get_stats(driver_count)
 
+# %%
 # Find outliers
 ride_count_outliers = Pyber.find_outliers(ride_count, ride_count_stats)
 fares_outliers = Pyber.find_outliers(fare_count, fare_stats)
 drivers_outliers = Pyber.find_outliers(driver_count, drivers_stats)
-breakpoint()
 
+# %% 
+# Plotting
 # # Bubble charts many
 # for fig, city_type in Pyber.bubble_charts(ride_count, fare_average, drivers_average):
 #     fig.savefig(output_path / f"ridesharing_{city_type}.jpeg", 
